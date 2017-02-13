c6a5e538b3458391d2deacb98013d3c79f77232a
vinh nguyen <vinh.nglx@gmail.com>
2017-02-13 23:14:48 +0800

    3rd: Test MadLib#display

--- madlibs/lib/mad_libs.rb
+++ madlibs/lib/mad_libs.rb
@@ -1,1 +1,14 @@
 require 'mad_libs/question'
+require 'mad_libs/question_collection'
+
+class MadLib
+  attr_reader :questions
+
+  def initialize(questions)
+    @questions = questions
+  end
+
+  def display
+    questions.data.first.content
+  end
+end
---
+++ madlibs/test/test_mad_libs.rb
@@ -0,0 +1,20 @@
+require 'helper'
+
+class TestMadLibs < MiniTest::Test
+  def test_display_question
+    questions = QuestionCollection.new(
+                  questions: [
+                    Question.new(content: "Question 1"),
+                    Question.new(content: "Question 2"),
+                    Question.new(content: "Question 3")
+                  ]
+                )
+
+    mad_libs = MadLib.new(questions)
+
+    question = mad_libs.display
+
+    assert_includes(["Question 1", "Question 2", "Question 3"], question)
+  end
+end
+
